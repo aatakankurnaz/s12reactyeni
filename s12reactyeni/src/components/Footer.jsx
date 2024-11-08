7ac3c67b4e832476150d9fe5dd86603f34a8f06c
aatakankurnaz <aatakankurnaz@gmail.com>
2024-11-08 09:22:00 +0300

    footer eklendi

--- s12reactyeni/src/components/Footer.jsx
+++ s12reactyeni/src/components/Footer.jsx
@@ -1,5 +1,29 @@
 export const Footer = () => {
     return (
-        <div className="h-screen">Footer</div>
+        <div className=" flex items-center justify-center font-inter">
+        <div className='w-[60%] h-[70%]  flex flex-col my-20  items-center'>
+           <h1 className="text-[#4731D3] font-bold text-5xl">Send me a message!</h1>
+           <h2 className="mt-8 text-center text-2xl">Got a question or proposal, or just want <br/>
+            to say hello? Go ahead.</h2>
+           <a href="" className="mt-2 text-[#4731D3] text-xl">atakankurnaz@gmail.com</a>
+            <nav className="flex gap-4 mt-3">
+                <a href="">
+                    <img src="/twitter 1.png" alt="" />
+                </a>
+                <a href="">
+                    <img src="/codepen 1.png" alt="" />
+                </a>
+                <a href="">
+                    <img src="/at-sign 1.png" alt="" />
+                </a>
+                <a href="">
+                    <img src="/instagram 1.png" alt="" />
+                </a>
+            </nav>
+            
+            
+        </div>
+
+    </div>
     )
 }
